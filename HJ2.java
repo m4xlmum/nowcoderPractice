b1821d618bcf323e69349f78c9a5cb4a58c106ba
m4xlmum <2994016494@qq.com>
2023-06-26 01:20:43 +0800

    HJ3

--- HJ2.java
+++ HJ2.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
+// 计算某字符出现次数
 public class HJ2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // 获取输入字符串
+        String str1 = scanner.nextLine();
+        // 获取输入字符
+        String str2 = scanner.nextLine();
+        // 将字符串中的字符替换，使用减法获取出现次数
+        int ans = str1.length() - str1.toUpperCase().replaceAll(str2.toUpperCase(), "").length();
 
+        // 输出结果
+        System.out.println(ans);
     }
 }
---
+++ HJ3.java
@@ -0,0 +1,26 @@
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+// 明明的随机数
+public class HJ3 {
+    public static void main(String[] args) {
+        // 创建接收器
+        Scanner sc = new Scanner(System.in);
+        // 获取个数
+        int n = sc.nextInt();
+        //创建TreeSet，可以进行去重排序
+        TreeSet set = new TreeSet();
+
+        // 获取数据
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+        // 输出结果
+        // System.out.println(set);
+        Iterator itSet = set.iterator();
+        while (itSet.hasNext()) {
+            System.out.println(itSet.next());
+        }
+    }
+}
