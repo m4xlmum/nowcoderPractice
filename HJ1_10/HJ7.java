085606c465f24e1556f1a8084a5cd63f0d2ac2d3
m4xlmum <2994016494@qq.com>
2023-06-26 22:33:24 +0800

    HJ7

---
+++ HJ1_10/HJ7.java
@@ -0,0 +1,11 @@
+package HJ1_10;
+
+import java.util.Scanner;
+
+public class HJ7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float n = scanner.nextFloat();
+        System.out.println(Math.round(n));  // 四舍五入输出
+    }
+}
